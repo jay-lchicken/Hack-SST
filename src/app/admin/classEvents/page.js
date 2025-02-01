481dc91adead3b00f554327ec9bb879992188fb1
Lai Hong Yu <joyjay606@gmail.com>
2025-02-01 15:03:20 +0800

    Added UI

--- src/app/admin/classEvents/page.js
+++ src/app/admin/classEvents/page.js
@@ -158,10 +158,24 @@ export default function Announcements() {
                 />
 
                 {/* Loading Animation */}
-                <div id="load" className="flex space-x-2 text-4xl">
-                    {["G", "N", "I", "D", "A", "O", "L"].map((letter, index) => (
-                        <div key={index}>{letter}</div>
-                    ))}
+                <div className={"flex flex-row"}>
+                    <div className="loader">
+                        <svg viewBox="0 0 80 80">
+                            <circle r="32" cy="40" cx="40" id="test"></circle>
+                        </svg>
+                    </div>
+
+                    <div className="loader triangle">
+                        <svg viewBox="0 0 86 80">
+                            <polygon points="43 8 79 72 7 72"></polygon>
+                        </svg>
+                    </div>
+
+                    <div className="loader">
+                        <svg viewBox="0 0 80 80">
+                            <rect height="64" width="64" y="8" x="8"></rect>
+                        </svg>
+                    </div>
                 </div>
             </div>
         );
