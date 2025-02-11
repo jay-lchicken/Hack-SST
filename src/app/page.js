8f8def43027810f51006a3680e4250c88c2d6c01
HONGYU-PROMAX <167660133+jay-lchicken@users.noreply.github.com>
2025-02-11 21:46:23 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -239,17 +239,17 @@ export default function Home() {
                 Hack@SST
               </a>
             </span>
-            registrations start around late Febuary and end by mid to late March
+            registrations start around Febuary and end by mid to late March
             to Sec 1s with a rough intake of 20-30 members every year
           </h1>
           <div class="flex flex-col items-center gap-3">
-            <button className="animated-button mt-4" >
+            <button className="animated-button mt-4"  onClick={() => window.location.href="https://hackatsst.fillout.com/t/7FKy1ukDdZus"}>
               <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                 ></path>
               </svg>
-              <span className="text">Registration Closed</span>
+              <span className="text">Registration Open</span>
               <span className="circle"></span>
               <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
                 <path
