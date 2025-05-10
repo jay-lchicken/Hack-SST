b2ed0bdcef5888ef8b0f49f6754e05d0ff4ae023
Lai Hong Yu <joyjay606@gmail.com>
2025-05-10 08:21:57 +0800

    Refactor route.js to simplify private key handling for Firebase Admin SDK

--- src/app/api/formChangeStatus/route.js
+++ src/app/api/formChangeStatus/route.js
@@ -10,7 +10,7 @@ export async function POST(request) {
             credential: admin.credential.cert({
                 projectId: process.env.FIREBASE_PROJECT_ID,
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Convert escaped \n back to actual newlines
+                privateKey: process.env.FIREBASE_PRIVATE_KEY, // Convert escaped \n back to actual newlines
             }),
         });
 
